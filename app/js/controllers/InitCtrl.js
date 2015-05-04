e6c12240e30a1104cfce39798249d8b71a178f96
jihusby <jihusby@gmail.com>
2015-05-04 13:17:12 +0200

    Initial commit of the skeleton app

---
+++ app/js/controllers/InitCtrl.js
@@ -0,0 +1,12 @@
+/**
+ * All initial values are declared in this file
+ *
+ */
+
+myApp.controller('InitCtrl', function InitCtrl($scope) {
+    $scope.init =
+        {   title: "iCore Skeleton App",
+            description: "This is the app description, located in the /controllers/InitCtrl.js file"
+        }
+
+});
